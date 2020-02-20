c6ac4e6c917d533b9f648005ecb1db394cfe496d
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-20 13:48:10 +0200

    q5 solve

--- app.js
+++ app.js
@@ -96,11 +96,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+ var mult = multArr.reduce((a,b)=>a*b,1)
+ return [mult,`The numbers ${multArr} have a product of ${mult}.`]
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
